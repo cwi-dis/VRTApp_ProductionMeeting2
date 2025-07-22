0e2ed97055d73ce80fb90a255ea47f74b05f5f3e
Sueyoon Lee <sueyoon.lee@hotmail.com>
2025-07-22 17:50:05 +0200

    scenechange prefab working all scenes except for training room

--- Assets/ProductionMeeting/Scripts/MeetingRoomController.cs
+++ Assets/ProductionMeeting/Scripts/MeetingRoomController.cs
@@ -1,19 +1,73 @@
 ﻿using UnityEngine;
 using VRT.Pilots.Common;
 using VRT.Core;
+using UnityEngine.SceneManagement;
 
 namespace VRT.Pilots.ProductionMeeting
 {
     public class MeetingRoomController : PilotController
     {
-        //public string sceneName = ""; // Name of the scene to load
-        public GameObject[] sceneChangeButton; // Reference to the button that will trigger the scene load
+       
+        public GameObject sceneChange;
 
-        public void LoadSceneName(string sceneName)
+
+        public void LoadSceneName()
         {
 
-            // Load the portal scene when the user clicks the button
-            LoadNewScene(sceneName);
+                if (sceneChange == null)
+                {
+                    Debug.LogWarning("sceneChange is not assigned.");
+                    return;
+                }
+
+                 Transform sceneOptions = sceneChange.transform.Find("ExampleSceneUI");
+                if (sceneOptions == null)
+                {
+                    Debug.LogWarning("ExampleSceneUI not found under sceneChange.");
+                    return;
+                }
+
+                Transform mediascape = sceneOptions.Find("Mediascape");
+                if (mediascape != null && mediascape.gameObject.activeSelf)
+                {
+                    if (SceneManager.GetActiveScene().name != "MediaScape_Stage")
+                        LoadNewScene("MediaScape_Stage");
+                    return;
+                }
+
+                Transform vrlobby = sceneOptions.Find("VRLobby");
+                if (vrlobby != null && vrlobby.gameObject.activeSelf)
+                {
+                    if (SceneManager.GetActiveScene().name != "VRLobby")
+                        LoadNewScene("TractionLobby");
+                    return;
+                }
+
+                Transform culture = sceneOptions.Find("5Dculture");
+                if (culture != null && culture.gameObject.activeSelf)
+                {
+                    if (SceneManager.GetActiveScene().name != "5DCultureHistoricalNew")
+                        LoadNewScene("5DCultureHistoricalNew");
+                    return;
+                }
+
+                Transform meeting = sceneOptions.Find("Meeting");
+                if (meeting != null && meeting.gameObject.activeSelf)
+                {
+                    if (SceneManager.GetActiveScene().name != "MeetingRoom")
+                        LoadNewScene("MeetingRoom");
+                    return;
+                }
+
+                Transform training = sceneOptions.Find("Training");
+                if (meeting != null && meeting.gameObject.activeSelf)
+                {
+                    if (SceneManager.GetActiveScene().name != "Training")
+                        LoadNewScene("Training");
+                    return;
+                }
+
+            
         }
 
     } 
