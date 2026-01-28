f51727be9f62f1647c3ec1be96026daa0d8c429c
Topinambur\dis <cwidisgroup@gmail.com>
2026-01-28 17:17:43 +0100

    Fixed the buttons (change size, reset costume) in the 5d culture room

--- Assets/5Dculture/Scripts/ChangeSize.cs
+++ Assets/5Dculture/Scripts/ChangeSize.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class ChangeSize : MonoBehaviour
 {
     public GameObject costume;
-    public float Scale = 0f;
+    public float Scale = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,8 @@ public class ChangeSize : MonoBehaviour
     // Update is called once per frame
     public void UpdateSize()
     {
-        costume.transform.localScale += new Vector3(Scale, Scale, Scale);
+        Vector3 scaleVector = new Vector3(Scale, Scale, Scale);
+        costume.transform.localScale = Vector3.Scale(costume.transform.localScale, scaleVector);
     }
 }
+
--- Assets/5Dculture/Scripts/ResetCostume.cs
+++ Assets/5Dculture/Scripts/ResetCostume.cs
@@ -7,14 +7,16 @@ public class ResetCostume : MonoBehaviour
     // Start is called before the first frame update
     public GameObject costume;
     private Quaternion originalRotation;
+    private Vector3 originalScale;
     void Start()
     {
         originalRotation = costume.transform.localRotation;   
+        originalScale = costume.transform.localScale;
     }
 
     public void Reset()
     {
-        costume.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        costume.transform.localScale = originalScale;
         costume.transform.localRotation = originalRotation;
     }
 }
